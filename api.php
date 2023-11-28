e95b6777330000b6165b500b887c9c1a62ea5fd0
Florian Chartier <fwchartier@gmail.com>
2023-11-28 10:02:46 +0100

    creation and verification of password work

--- api.php
+++ api.php
@@ -20,11 +20,23 @@ session_start();
 checkCSRFAsync($data);
 checkXSS($data);
 
-// Connexion with an existing account
+// Creation of an account
 if (isset($data['action']) && $data['action'] === 'inscription' && isset($data['email']) && strlen($data['email']) > 0 && isset($data['pwd']) && strlen($data['pwd']) > 5) {
     try {
+        $query = $dbCo->prepare('SELECT email FROM users;');
+        $query->execute();
+        $result = $query->fetchAll();
+        foreach($result as $i) {
+            if ($i['email'] === $data['email']) {
+                echo json_encode([
+                    'result' => false,
+                    'error' => 'Cette adresse existe déjà.'
+                ]);
+                exit;
+            };
+        };
         $dbCo->beginTransaction();
-        $query = $dbCo->prepare("INSERT INTO users SET email = :email, hashed_pwd = :hashed_pwd;");
+        $query = $dbCo->prepare("INSERT INTO users(email, hashed_pwd) VALUES (:email, :hashed_pwd);");
         $isQueryOk = $query->execute([
             'email' => $data['email'],
             'hashed_pwd' => password_hash($data['pwd'], PASSWORD_DEFAULT)
@@ -37,6 +49,45 @@ if (isset($data['action']) && $data['action'] === 'inscription' && isset($data['
             echo json_encode([
                 'result' => false,
                 'error' => 'Problème lors de la création du compte.'
+            ]);
+        }
+    } catch (Exception $e) {
+        $dbCo->rollBack();
+        echo json_encode([
+            'result' => false,
+            'error' => 'Une erreur s\'est produite : ' . $e->getMessage()
+        ]);
+    }
+}
+
+// Connexion with existing account
+else if (isset($data['action']) && $data['action'] === 'connexion' && isset($data['email']) && strlen($data['email']) > 0 && isset($data['pwd']) && strlen($data['pwd']) > 5) {
+    try {
+        $dbCo->beginTransaction();
+        $query = $dbCo->prepare('SELECT hashed_pwd FROM users WHERE email = :email;');
+        $isQueryOk = $query->execute([
+            'email' => $data['email']
+        ]);
+        if ($isQueryOk) {
+            $result = $query->fetch();
+            $dbCo->commit();
+            if (!password_verify($data['pwd'], $result['hashed_pwd'])) {
+                echo json_encode([
+                    'result' => false,
+                    'error' => 'Ce n\'est pas le bon mot de passe.'
+                ]);
+                exit;
+            } else {
+                echo json_encode([
+                    'result' => true,
+                    'notif' => 'Connexion ok.'
+                ]);
+            }
+        } else {
+            $dbCo->rollBack();
+            echo json_encode([
+                'result' => false,
+                'error' => 'Problème lors de la connexion au compte.'
             ]);
         }
     } catch (Exception $e) {
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+document.getElementById('create__btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    document.getElementById('inscription').classList.remove('hidden');
+    document.getElementById('connexion').classList.add('hidden');
+})
+
 document.getElementById('insc__btn').addEventListener('click', function (event) {
     event.preventDefault();
     const data = {
@@ -14,10 +20,24 @@ document.getElementById('insc__btn').addEventListener('click', function (event) 
         .catch(error => {
             console.error("Error :", error);
         });
-})
+});
 
-document.getElementById('create__btn').addEventListener('click', function (event) {
+document.getElementById('conn__btn').addEventListener('click', function (event) {
     event.preventDefault();
-    document.getElementById('inscription').classList.remove('hidden');
-    document.getElementById('connexion').classList.add('hidden');
-})
+    const data = {
+        action: 'connexion',
+        token: getToken(),
+        email: document.getElementById('conn__email').value,
+        pwd: document.getElementById('conn__pwd').value
+    };
+    fetchApi('POST', data)
+        .then(data => {
+            console.log(data);
+            if (data['result'] === true) {
+                console.log('On connecte la base de données.')
+            }
+        })
+        .catch(error => {
+            console.error("Error :", error);
+        });
+});
