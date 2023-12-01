9a2e38aabc5b500ca3931540eb3b5f5a467cffba
Oyannax <oyannalopez14@gmail.com>
2023-12-01 09:02:27 +0100

    display new grower

--- manager.php
+++ manager.php
@@ -5,6 +5,7 @@ include_once 'includes/_functions.php';
 
 session_start();
 getToken();
+$_SESSION['id_person'] = 8;
 ?>
 
 <!DOCTYPE html>
@@ -14,46 +15,79 @@ getToken();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/manager.css">
     <title>CreAmap</title>
 </head>
 
 <body class="manager-body">
     <header class="manager-header">
+        <?php
+        $displayName = $dbCo->prepare('SELECT firstname FROM person WHERE id_person = :id;');
+        $displayName->execute([
+            'id' => $_SESSION['id_person']
+        ]);
+        $name = $displayName->fetchColumn();
+        ?>
         <div class="header-content">
             <h1 class="hidden">CreAmap</h1>
             <img src="assets/img/creamap_2_-removebg-preview.png" alt="CreAmap second logo" class="scnd-logo">
-            <h2 class="welcome-txt">Bienvenue (Prénom)</h2>
+            <h2 class="welcome-txt">Bienvenue <?= $name ?></h2>
         </div>
     </header>
     <main class="manager-main">
-        <section class="display">
-            <div class="title-cntnr">
-                <div class="title">
-                    <h3 class="title-txt">(Nom Entreprise)</h3>
-                </div>
-                <button class="product-btn">
-                    <div class="add-product">
-                        <img src="assets/img/plus-solid.svg" alt="plus solid icon" class="add-icon">
+        <?php
+        $query = $dbCo->prepare('SELECT id_amap_user FROM amap_user WHERE id_person = :id;');
+        $query->execute([
+            'id' => $_SESSION['id_person']
+        ]);
+        $idUser = $query->fetchColumn();
+
+        $displayGrowers = $dbCo->prepare('SELECT id_producer, producer_name FROM producer JOIN manage USING (id_producer) WHERE id_amap_user = :id;');
+        $displayGrowers->execute([
+            'id' => $idUser
+        ]);
+        $growers = $displayGrowers->fetchAll();
+
+        foreach ($growers as $grower) {
+        ?>
+            <section class="display">
+                <div class="title-cntnr">
+                    <div class="title">
+                        <h3 class="title-txt"><?= $grower['producer_name'] ?></h3>
                     </div>
-                </button>
-            </div>
-            <div class="list-cntnr">
-                <ul class="list-content">
-                    <li class="product-cntnr">
-                        <h4 class="product-title">(Qty ttl) (Nom Produit)</h4>
-                        <div class="icons-cntnr">
-                            <button class="edit-btn">
-                                <img src="assets/img/pencil-solid.svg" alt="pencil solid logo" class="icon">
-                            </button>
-                            <button class="delete-btn">
-                                <img src="assets/img/trash-can-solid.svg" alt="trash can solid logo" class="icon">
-                            </button>
+                    <button class="product-btn">
+                        <div class="add-product">
+                            <img src="assets/img/plus-solid.svg" alt="plus solid icon" class="add-icon">
                         </div>
-                    </li>
-                </ul>
-            </div>
-        </section>
+                    </button>
+                </div>
+                <div class="list-cntnr">
+                    <ul class="list-content">
+                        <?php
+                        $displayProducts = $dbCo->prepare('SELECT id_product, product_name, SUM(quantity) AS ttl_qty FROM product JOIN commitment USING (id_product) JOIN subscribe USING (id_commitment) WHERE id_producer = :id GROUP BY id_commitment;');
+                        $displayProducts->execute([
+                            'id' => $grower['id_producer']
+                        ]);
+                        $products = $displayProducts->fetchAll();
+
+                        foreach ($products as $product) {
+                        ?>
+                            <li data-id-product="<?= $product['id_product'] ?>" class="product-cntnr">
+                                <h4 class="product-title"><?= $product['ttl_qty'] ?> <?= $product['product_name'] ?></h4>
+                                <div class="icons-cntnr">
+                                    <button class="edit-btn" id="editBtn">
+                                        <img src="assets/img/pencil-solid.svg" alt="pencil solid logo" class="icon">
+                                    </button>
+                                    <button class="delete-btn" id="deleteBtn">
+                                        <img src="assets/img/trash-can-solid.svg" alt="trash can solid logo" class="icon">
+                                    </button>
+                                </div>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </section>
+        <?php } ?>
         <section class="add">
             <button class="grower-btn" id="displayGrowerForm">
                 <div class="add-grower">
@@ -87,8 +121,20 @@ getToken();
             </a>
         </nav>
     </main>
-    <script src="./assets/js/functions.js"></script>
-    <script src="manager.js"></script>
+    <template id="growerTemplate">
+        <div class="title-cntnr">
+            <div class="title">
+                <h3 class="title-txt" data-content="text"></h3>
+            </div>
+            <button class="product-btn">
+                <div class="add-product">
+                    <img src="assets/img/plus-solid.svg" alt="plus solid icon" class="add-icon">
+                </div>
+            </button>
+        </div>
+    </template>
+    <script src="assets/js/functions.js"></script>
+    <script src="assets/js/manager.js"></script>
 </body>
 
 </html>
